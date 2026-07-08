c221ba3a90839b3e0506cdde66dfcdc3f830f8c8
amandamgc <amandaa.mgc@hotmail.com>
2026-07-07 21:21:48 -0600

    Update main.tf

--- infra/modules/app_server/main.tf
+++ infra/modules/app_server/main.tf
@@ -46,13 +46,26 @@ resource "aws_instance" "app_server" {
   instance_type = var.instance_type
   subnet_id     = var.public_subnet_id
   key_name      = var.key_name
-  user_data = <<-EOF
-    #!/bin/bash
-    yum update -y
-    amazon-linux-extras install nginx1 -y
-    systemctl start nginx
-    systemctl enable nginx
-  EOF
+user_data = <<-EOF
+  #!/bin/bash
+  yum update -y
+  amazon-linux-extras install nginx1 -y
+  systemctl start nginx
+  systemctl enable nginx
+
+  # Instalar Node.js
+  curl -sL https://rpm.nodesource.com/setup_18.x | bash -
+  yum install -y nodejs
+
+  # Clonar repo y correr backend
+  cd /home/ec2-user
+  git clone -b backend-udl https://github.com/TakeshiNR/MEVN_Terraform_1055C.git
+  cd MEVN_Terraform_1055C/backend
+  echo "PORT=3000" > .env
+  echo "MONGO_URI=mongodb://10.0.2.8:27017/gestor_tareas" >> .env
+  npm install
+  npm start &
+EOF
 
   vpc_security_group_ids = [aws_security_group.app_server.id]
 
